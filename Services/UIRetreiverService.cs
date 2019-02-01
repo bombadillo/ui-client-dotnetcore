c91dc625e910f0f1284cb8f2410ef8f09f4a3097
bombadillo <mumfordnsons@gmail.com>
2019-02-01 13:04:17 +0000

    Add static for ComponentType in UI request

--- Services/UIRetreiverService.cs
+++ Services/UIRetreiverService.cs
@@ -10,7 +10,7 @@ namespace ui_client_dotnetcore.Services
     {
       using (var client = new HttpClient())
       {
-        var url = "http://localhost:3000/component/getComponent/2bca1032-4700-45db-975c-30dae65de7ae";
+        var url = "http://localhost:3000/component/getComponent/2bca1032-4700-45db-975c-30dae65de7ae/static";
 
         client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("94288c19-cda4-48bb-88a6-6b73f27d693a");
         var response = await client.GetAsync(url);
